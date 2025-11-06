840ab587175a73e498061223713ac815870a63ea
DevGege <86485961+GeovaneParedes@users.noreply.github.com>
2025-11-06 13:25:45 -0300

    close #8
    * added gitignore #8
    
    * Set up project initial organization and metadata\n\n-Add setup.py to turn installable\n-Add __main_.py for executable\n-Add __init__.py for package\n\nclose #8
    
    * Set
    
    ---------
    
    Co-authored-by: GeovaneParedes <geovaneparedes@gmail.com>

---
+++ dundie/__main__.py
@@ -0,0 +1,1 @@
+print("Execunting dundie package...")
---
+++ setup.py
@@ -0,0 +1,10 @@
+# setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Rewards Point System for Dunder Mifflin",
+    author="Develop Geovane Paredes",
+    packages=find_packages(),
+)
