df79be98a910b03c322888aecd057770c03fac62
DevGege <86485961+GeovaneParedes@users.noreply.github.com>
2025-11-14 20:01:03 -0300

    Organized tests (#13)
    * Organized tests
    * Add CI GHA
    
    ---------
    
    Co-authored-by: GeovaneParedes <geovaneparedes@gmail.com>
    closes #10

--- setup.py
+++ setup.py
@@ -1,14 +1,40 @@
+import os
 from setuptools import setup, find_packages
+
+def read(*paths):
+    """Read the contents of a text file safely
+    >>> read("dundie", "VERSIOM)
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+    rootpath = os.path.dirname(__file__)
+    filepath = os.path.join(rootpath, *paths)
+    with open(filepath) as file_:
+        return file_.read().strip()
+
+
+def read_requirements(path):
+    """Return a list of requirements from a text file"""
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(("#", "git", '"', "-"))
+    ]
 
 setup(
     name="dundie",
     version="0.1.0",
     description="Rewards Point System for Dunder Mifflin",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
     author="Develop Geovane Paredes",
+    python_requires=">=3.8",
     packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "dundie = dundie.__main__:main"
-        ]
+    entry_points={"console_scripts": ["dundie = dundie.__main__:main"]},
+    install_requires=read_requirements("requirements.txt"),
+    extras_requires={
+        "test": read_requirements("requirements.test.txt"),
+        "dev": read_requirements("requirements.dev.txt")
     }
 )
